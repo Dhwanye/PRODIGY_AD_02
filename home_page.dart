0dd08e548c2ad12ea57919f817c761ed5e865d06
Dhwanye <96939158+Dhwanye@users.noreply.github.com>
2024-08-05 22:02:03 +0530

    Update home_page.dart

--- home_page.dart
+++ home_page.dart
@@ -2,7 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:todo_app/utils/todo_list.dart';
 
 class HomePage extends StatefulWidget{
-   HomePage({super.key});
+  HomePage({super.key});
 
   @override
   State<HomePage> createState() => _HomePageState();
@@ -11,44 +11,44 @@ class HomePage extends StatefulWidget{
 class _HomePageState extends State<HomePage> {
   final _controller=TextEditingController();
   List toDolist=[
-    ['Learn Web development',false],
-    ['Eat',false]
+    ['Buy grocery',false],
+    ['Books',false]
 
   ];
 
-   void deleteTask(int index)
-   {
-     setState(() {
-       toDolist.removeAt(index);
-     });
+  void deleteTask(int index)
+  {
+    setState(() {
+      toDolist.removeAt(index);
+    });
 
-   }
-   void checkBoxChanged(int index) {
-     toDolist[index][1] = !toDolist[index][1];
-   }
-   void savedTask(){
-     setState(() {
-       toDolist.add([_controller.text,false]);
-       _controller.clear();
-     });
-   }
+  }
+  void checkBoxChanged(int index) {
+    toDolist[index][1] = !toDolist[index][1];
+  }
+  void savedTask(){
+    setState(() {
+      toDolist.add([_controller.text,false]);
+      _controller.clear();
+    });
+  }
 
-@override
- Widget build(BuildContext context)
- {
-          return Scaffold(
-            backgroundColor: Colors.deepPurple.shade300,
-            appBar: AppBar(
-              title: const Text(
-                'ToDO App'
-              ),
-              backgroundColor: Colors.deepPurple,
-              foregroundColor: Colors.white,
-            ),
-            body: ListView.builder(
-                itemCount: toDolist.length,
-                itemBuilder: (BuildContext context,index)
-                {
+  @override
+  Widget build(BuildContext context)
+  {
+    return Scaffold(
+        backgroundColor: Colors.blueGrey,
+        appBar: AppBar(
+          title: const Text(
+              'ToDO App'
+          ),
+          backgroundColor: Colors.black38,
+          foregroundColor: Colors.white,
+        ),
+        body: ListView.builder(
+            itemCount: toDolist.length,
+            itemBuilder: (BuildContext context,index)
+            {
               return TodoList(
                 taskName: toDolist[index][0],
                 taskCompleted: toDolist[index][1],
@@ -56,39 +56,41 @@ class _HomePageState extends State<HomePage> {
                 deleteFunction: (context) => deleteTask(index),
               );
             } ),
-              floatingActionButton: Row(
-               children:[
-                 Expanded(
-                     child: Padding(
-                       padding: const EdgeInsets.symmetric(horizontal: 20.0),
-                       child: TextField(
-                         controller: _controller ,
-                         decoration: InputDecoration(
-                           hintText: 'Add a new todo items',
-                           filled: true,
-                           fillColor: Colors.deepPurple.shade200,
-                           enabledBorder: OutlineInputBorder(
-                             borderSide: BorderSide(
-                               color: Colors.deepPurple,
-                             ),
-                             borderRadius: BorderRadius.circular(15)
-                           ),
-                          focusedBorder: OutlineInputBorder(
-                               borderSide: BorderSide(
-                                 color: Colors.deepPurple,
-                               ),
-                               borderRadius: BorderRadius.circular(15)
-                           ),
-                         ) ,
-                       ),
-                     ),
-                 ),
+        floatingActionButton: Row(
+            children:[
+              Expanded(
+                child: Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 20.0),
+                  child: TextField(
+                    controller: _controller ,
+                    decoration: InputDecoration(
+                      hintText: 'Enter here',
+                      filled: true,
+                      fillColor: Colors.blueGrey.shade400,
+                      enabledBorder: OutlineInputBorder(
+                          borderSide: BorderSide(
+                            color: Colors.black38,
+                          ),
+                          borderRadius: BorderRadius.circular(15)
+                      ),
+                      focusedBorder: OutlineInputBorder(
+                          borderSide: BorderSide(
+                            color: Colors.black38,
+                          ),
+                          borderRadius: BorderRadius.circular(15)
+                      ),
+                    ) ,
+                  ),
+                ),
+              ),
 
-             FloatingActionButton(
+              FloatingActionButton(
                 onPressed:savedTask,
-               child: const Icon(Icons.add),
-              ),]
-              ));
- }
-}
+                child: const Icon(Icons.add),
 
+              ),
+
+            ]
+        ));
+  }
+}
